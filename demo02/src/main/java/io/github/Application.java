eb01447d7088a0675d5ee9986f03a17795262e0c
shenjy <markshen1992@gmail.com>
2022-04-30 09:51:38 +0800

    spring security.

--- demo02/src/main/java/io/github/Application.java
+++ demo02/src/main/java/io/github/Application.java
@@ -1,11 +1,15 @@
 package io.github;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(Application.class, args);
     }
 }
